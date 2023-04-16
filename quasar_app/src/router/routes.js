3f69a6140962ee647e44c2edd57a7e0d3a527198
phantom <akashmelkeri23@gmail.com>
2023-04-16 09:42:57 +0530

    route

--- quasar_app/src/router/routes.js
+++ quasar_app/src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/doraemon', component: () => import('pages/Doraemon.vue') }
     ]
   },
 
